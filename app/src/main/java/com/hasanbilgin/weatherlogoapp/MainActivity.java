f93024f04f7c8b3910aa94d058186e9a8f2d6579
Hasan Bilgin <hasanbilgintr@gmail.com>
2024-01-10 04:57:42 +0300

    first commit

---
+++ app/src/main/java/com/hasanbilgin/weatherlogoapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hasanbilgin.weatherlogoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println();
+    }
+}
